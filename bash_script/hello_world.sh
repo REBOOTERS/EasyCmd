ef87c0999e65f9f662b7ce75691d8e1aacc64843
root <root@Engineer.localdomain>
2022-07-17 13:49:00 +0800

    test bash

---
+++ bash_script/hello_world.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+middle=$(grep startActivity log.txt)
+#echo $middle
+
+get_method_time() {
+  result=$(grep $1 log.txt | awk '{print $2}')
+  return "$result"
+}
+
+
+
+start_activity_time=$(grep startActivity log.txt | awk '{print $1,$2}')
+create_time=$(grep onCreate log.txt | awk '{print $1,$2}')
+resume_time=$(grep onResume log.txt | awk '{print $1,$2}')
+
+print_method_time() {
+  echo "start_activity_time = $start_activity_time"
+  echo "        create_time = $create_time"
+  echo "        resume_time = $resume_time"
+}
+
+#print_method_time
+
+start_activity_time_stamp=$(date +%s%N -d "$start_activity_time")
+create_time_stamp=$(date +%s%N -d "$create_time")
+resume_time_stamp=$(date +%s%N -d "$resume_time")
+
+print_method_timestamp() {
+  echo `($start_activity_time_stamp)/1000000`
+  echo $create_time_stamp
+  echo $resume_time_stamp
+}
+
+print_method_timestamp
+
+
+create_duration=$(($(date +%s%N -d "$create_time")/1000000 - $(date +%s%N -d "$start_activity_time")/1000000))
+resume_duration=$(($(date +%s%N -d "$resume_time")/1000000 - $(date +%s%N -d "$start_activity_time")/1000000))
+echo "create_duration= $create_duration"
+echo "resume_duration= $resume_duration"
